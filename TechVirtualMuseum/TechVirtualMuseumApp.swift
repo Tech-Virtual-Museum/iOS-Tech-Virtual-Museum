a56c2e2b3c294408f78fe766626397d4250c4a85
Nahima Ortega <nahimaortega@gmail.com>
2022-11-02 23:43:08 +0000

    Initial Commit

---
+++ TechVirtualMuseum/TechVirtualMuseumApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechVirtualMuseumApp.swift
+//  TechVirtualMuseum
+//
+//  Created by Nahima Ortega on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TechVirtualMuseumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
