988877d3faa453253751c49d7b7da30fa275b066
Nahima Ortega <nahimaortega@gmail.com>
2022-12-05 22:00:27 +0000

    image carousel v1

--- TechVirtualMuseum/View/CarouselView.swift
+++ TechVirtualMuseum/View/CarouselView.swift
@@ -13,17 +13,18 @@ struct Item: Identifiable {
     let image: Image
 }
 
-let roles = ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FImage&psig=AOvVaw3p0gSlnw_u2hzsF7UpNe9a&ust=1670361186167000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKiVvrqy4_sCFQAAAAAdAAAAABAE", "https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/2.jpg"]
+let roles = ["LaunchImage", "LogoWithoutText"]
 
 struct CarouselView: View {
     let items: [Item] = roles.map { Item(image: Image($0)) }
+    
         
         var body: some View {
             ACarousel(items) { item in
                 item.image
                     .resizable()
                     .scaledToFill()
-                    .frame(height: 300)
+                    .frame(height: 150)
                     .cornerRadius(30)
             }
             .frame(height: 300)
