8f2e47697c94bc7c2232af148d7d0dcc31a3c355
Nahima Ortega <nahimaortega@gmail.com>
2022-12-10 18:53:53 +0000

    custom cells

---
+++ TechVirtualMuseum/Model/CustomNewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNewCell.swift
+//  TechVirtualMuseum
+//
+//  Created by Nahima Ortega on 10/12/22.
+//
+
+import UIKit
+
+class CustomNewCell: UITableViewCell {
+    
+    @IBOutlet weak var eventTitle: UILabel!
+    @IBOutlet weak var eventDate: UILabel!
+    @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var eventHour: UILabel!
+    
+}
