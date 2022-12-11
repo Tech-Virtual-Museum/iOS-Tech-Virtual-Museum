70e2b39125b0170d17c77ee13f00bf7113ce41d2
Nahima Ortega <nahimaortega@gmail.com>
2022-12-11 18:17:26 +0000

    search works

---
+++ TechVirtualMuseum/Controller/EventDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EventDetailsViewController.swift
+//  TechVirtualMuseum
+//
+//  Created by Nahima Ortega on 11/12/22.
+//
+
+import UIKit
+
+class EventDetailsViewController: UIViewController {
+    
+    var selectedItem: [String: Any]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(selectedItem)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TechVirtualMuseum/Controller/EventViewController.swift
+++ TechVirtualMuseum/Controller/EventViewController.swift
@@ -9,12 +9,23 @@ import UIKit
 import FirebaseFirestore
 
 
-class EventViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class EventViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+    }
+    
+
     var firestoreService = FirestoreService()
     
     @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
     var items: [[String: Any]] = []
+    var data: [String] = []
+    
+    var filteredItems: [[String: Any]] = []
+    let searchController = UISearchController(searchResultsController: nil)
     
     
     override func viewDidLoad() {
@@ -23,45 +34,39 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
 
         // Do any additional setup after loading the view.
         
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
         self.firestoreService.getCollection(collectionId: "eventos") {
             (error, collectionData) in
             for document in collectionData {
                 //print("\(document.documentID) => \(document.data())")
                 self.items.append(document.data())
             }
+            self.filteredItems = self.items
             self.tableView.reloadData()
         }
         
         view.addSubview(tableView)
-            tableView.translatesAutoresizingMaskIntoConstraints = false
-            let tableViewTopConstraint = tableView.topAnchor.constraint(equalTo: view.topAnchor)
-            tableViewTopConstraint.constant = +150
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        let tableViewTopConstraint = tableView.topAnchor.constraint(equalTo: view.topAnchor)
+        tableViewTopConstraint.constant = +160
         
-            let tableViewLeadingConstraint = tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
-            tableViewLeadingConstraint.constant = +10
-        
-            let tableViewTrailingConstraint = tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-            tableViewTrailingConstraint.constant = -10
-            NSLayoutConstraint.activate([
-              tableViewTopConstraint,
-              tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-              tableViewLeadingConstraint,
-              //tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-              //tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-            ])
+        let tableViewLeadingConstraint = tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        NSLayoutConstraint.activate([
+            tableViewTopConstraint,
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableViewLeadingConstraint,
+        ])
 
-            // Set the view controller as the data source and delegate of the table view
-            tableView.dataSource = self
-            tableView.delegate = self
+        tableView.dataSource = self
+        tableView.delegate = self
 
-            // Register a cell class for the table view
-            tableView.register(CustomNewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.register(CustomNewCell.self, forCellReuseIdentifier: "Cell")
         
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
-      }
 
       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CustomNewCell", for: indexPath) as! CustomNewCell
@@ -91,12 +96,13 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
               }
               task.resume()
           }
+          
         return cell
       }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
-        return 115
+        return 90 + 2 * 5
     }
     
     func tableView(_ tableView: UITableView, widthForRowAt indexPath: IndexPath) -> CGFloat {
@@ -104,13 +110,37 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
         return 200
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return filteredItems.count
+    }
+    
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cell.layer.cornerRadius = 20
-        cell.layoutMargins = UIEdgeInsets(top: 20, left: 16, bottom: 20, right: 16)
-        cell.separatorInset = UIEdgeInsets(top: 20, left: 16, bottom: 20, right: 16)
         cell.preservesSuperviewLayoutMargins = false
-        cell.layer.borderWidth = 20
-        cell.layer.borderColor = UIColor.clear.cgColor
+        cell.layer.borderWidth = 0
+        cell.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            // Create an instance of the view controller you want to navigate to
+            let vc = EventDetailsViewController()
+            
+            // Set any properties on the view controller that you want to pass
+            // along to the next view controller
+            vc.selectedItem = items[indexPath.row]
+            
+            // Use the navigation controller to push the view controller onto the stack
+            navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredItems = searchText.isEmpty ? items : items.filter { (item: [String: Any]) -> Bool in
+                // Extract the "name" property from the item dictionary
+                let itemName = item["name"] as! String
+                // If itemName matches the searchText, return true to include it
+            return itemName.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+            }
+        tableView.reloadData()
     }
     
 
