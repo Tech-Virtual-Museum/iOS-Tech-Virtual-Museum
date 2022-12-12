5474d8d4d9b16d2653b5e6b1a91eee5cd426d35d
Nahima Ortega <nahimaortega@gmail.com>
2022-12-12 00:29:26 +0000

    event details with images done

--- TechVirtualMuseum/Controller/EventDetailsViewController.swift
+++ TechVirtualMuseum/Controller/EventDetailsViewController.swift
@@ -26,6 +26,23 @@ class EventDetailsViewController: UIViewController {
         eventHour.text = selectedItem?["hour"] as? String
         eventPricing.configuration?.title = selectedItem?["pricing"] as? String
         eventDescription.text = selectedItem?["description"] as? String
+        if let url = URL(string: selectedItem?["largerImgUrl"] as! String) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil {
+                    // Make sure the data is not nil
+                    guard let data = data else { return }
+
+                    // Create the image on the main thread
+                    DispatchQueue.main.async {
+                        self.eventImage.image = UIImage(data: data)
+                    }
+                } else {
+                    print("Failed to load the data: \(error!)")
+                }
+            }
+            task.resume()
+        }
         // Do any additional setup after loading the view.
     }
     
