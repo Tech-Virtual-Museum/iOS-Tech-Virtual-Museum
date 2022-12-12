5c078853dcac22b3b32c631775fe52a0e12f1ade
Nahima Ortega <nahimaortega@gmail.com>
2022-12-12 18:00:48 +0000

    purchase storyboards done

--- TechVirtualMuseum/Controller/PurchaseViewController.swift
+++ TechVirtualMuseum/Controller/PurchaseViewController.swift
@@ -8,11 +8,35 @@
 import UIKit
 
 class PurchaseViewController: UIViewController {
-
+    
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var hourCollectionView: UICollectionView!
+    @IBOutlet weak var juniorStepperLbl: UILabel!
+    @IBOutlet weak var studentStepperLbl: UILabel!
+    @IBOutlet weak var adultStepperLbl: UILabel!
+    @IBOutlet weak var seniorStepperLbl: UILabel!
+    @IBOutlet weak var juniorOrderLbl: UILabel!
+    @IBOutlet weak var studentOrderLbl: UILabel!
+    @IBOutlet weak var adultOrderLbl: UILabel!
+    @IBOutlet weak var seniorOrderLbl: UILabel!
+    @IBOutlet weak var studentStepper: UIStepper!
+    @IBOutlet weak var adultStepper: UIStepper!
+    @IBOutlet weak var seniorStepper: UIStepper!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var juniorStepper: UIStepper!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+      print("Stepper value changed to: \(sender.value)")
     }
     
 
