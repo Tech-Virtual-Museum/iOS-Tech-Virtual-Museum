33d91d7d25dfd4a1ea86eb03d7a4cff6c7bc8bf9
Nahima Ortega <nahimaortega@gmail.com>
2022-12-19 17:03:11 +0000

    added new views in storyboard

---
+++ TechVirtualMuseum/Controller/PurchaseEventViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PurchaseEventViewController.swift
+//  TechVirtualMuseum
+//
+//  Created by Nahima Ortega on 19/12/22.
+//
+
+import UIKit
+
+class PurchaseEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
